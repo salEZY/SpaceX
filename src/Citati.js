8819181562f8dcc51456aecf03916848555ac3c3
salEZY <bobicaleks@yahoo.com>
2020-05-21 19:37:08 +0200

    start on voting hook

--- src/Citati.js
+++ src/Citati.js
@@ -3,12 +3,16 @@ import React, { useState } from 'react'
 
 const Citati = quotes => {
   const [selected, setSelected] = useState(0)
+  const [votes,setVotes] = useState(0)
 
   let citati = quotes.quotes
   return(
   <div>
   <p>{citati[selected]}</p>
+  <p>{votes}</p>
   <button onClick={() => setSelected((selected + 1) % citati.length)}>Next</button>
+  <button onClick={() => setVotes(votes + 1)}>Add Vote</button>
+  
   </div>
   )
 }
