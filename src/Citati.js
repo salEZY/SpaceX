9531d7d512f3936fdd9c9691623a98408642f7b5
AleksBobic <bobicaleks@yahoo.com>
2020-05-23 19:09:22 +0200

    state exercises

--- src/Citati.js
+++ src/Citati.js
@@ -1,20 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
+const Citati = (quotes) => {
+  let citati = quotes.quotes;
 
-const Citati = quotes => {
-  const [selected, setSelected] = useState(0)
-  const [votes,setVotes] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(citati.length).fill(0));
+  const [best, setBest] = useState(0);
 
-  let citati = quotes.quotes
-  return(
-  <div>
-  <p>{citati[selected]}</p>
-  <p>{votes}</p>
-  <button onClick={() => setSelected((selected + 1) % citati.length)}>Next</button>
-  <button onClick={() => setVotes(votes + 1)}>Add Vote</button>
-  
-  </div>
-  )
-}
+  const addPoint = () => {
+    const tmp = [...votes];
+    tmp[selected]++;
+    setVotes(tmp);
+  };
 
-export default Citati
+  const checkMostVoted = () => {
+    votes[selected] >= votes[best] ? setBest(selected) : setBest(best);
+  };
+
+  return (
+    <div>
+      <div style={{ borderBottom: "5px solid blue" }}>
+        {citati.map((citat) => (
+          <p key={citat}>{citat}</p>
+        ))}
+      </div>
+      <p>{citati[selected]}</p>
+      <p>{votes[selected]}</p>
+      <button onClick={() => setSelected((selected + 1) % citati.length)}>
+        Next
+      </button>
+      <button
+        onClick={() => {
+          addPoint();
+          checkMostVoted();
+        }}
+      >
+        Add Vote
+      </button>
+      <h2>Citat sa najvise glasova</h2>
+      <p>{citati[best]}</p>
+      <p>{votes[best]}</p>
+    </div>
+  );
+};
+
+export default Citati;
