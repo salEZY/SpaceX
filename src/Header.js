85d87d1f6cbcc8d9d8396bd05316c62ce0aac80e
AleksBobic <bobicaleks@yahoo.com>
2020-05-28 12:04:25 +0200

    refactor header

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,20 @@ const Header = ({ name, founder, summary }) => {
   return (
     <div className="header">
       <h1>
-        {name}
-        <i className="fas fa-rocket" style={{ marginLeft: "5px" }}></i> -{" "}
-        {founder}
-        <i className="fas fa-user-astronaut" style={{ marginLeft: "10px" }}></i>
+        <a
+          href="https://www.spacex.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {name} <i className="fas fa-rocket" style={{ marginLeft: "5px" }}></i>
+        </a>
+        <span>
+          {` ${founder}`}
+          <i
+            className="fas fa-user-astronaut"
+            style={{ marginLeft: "10px" }}
+          ></i>
+        </span>
       </h1>
       <p>{summary}</p>
     </div>
