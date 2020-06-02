f322f6fa2465a83701c5587b15217c8c048a0ba4
AleksBobic <bobicaleks@yahoo.com>
2020-06-02 20:57:43 +0200

    Updates

---
+++ precache-manifest.66c4be6d9e19b943c57cd256e7a90587.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aa69441572a8814b0e1abbe33e61ae",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "69dd2f89bff719509855",
+    "url": "/SpaceX/static/css/main.6b7391ad.chunk.css"
+  },
+  {
+    "revision": "ca85b5129d4584f95a40",
+    "url": "/SpaceX/static/js/2.8795ec8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpaceX/static/js/2.8795ec8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69dd2f89bff719509855",
+    "url": "/SpaceX/static/js/main.9f18c08d.chunk.js"
+  },
+  {
+    "revision": "2ac9ea2e5f7a388b805b",
+    "url": "/SpaceX/static/js/runtime-main.d5725359.js"
+  },
+  {
+    "revision": "42c7976bedfceccda9e631a13760a85e",
+    "url": "/SpaceX/static/media/juliussansone-regular-webfont.42c7976b.woff"
+  },
+  {
+    "revision": "dab43bc9264df28a19d312eabd74ebb6",
+    "url": "/SpaceX/static/media/juliussansone-regular-webfont.dab43bc9.woff2"
+  },
+  {
+    "revision": "8330aa112d90f38f6d6bfa6d7286708a",
+    "url": "/SpaceX/static/media/rez-webfont.8330aa11.woff"
+  },
+  {
+    "revision": "b3c60dee20648de0873952e3bc1047e0",
+    "url": "/SpaceX/static/media/rez-webfont.b3c60dee.woff2"
+  },
+  {
+    "revision": "64c5ce5b6f303616b7f0ba7bb2587b31",
+    "url": "/SpaceX/static/media/rocket.64c5ce5b.jpg"
+  }
+]);
